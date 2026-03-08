2e59859734f3381a6ddef1e4929f9c0edfce9cb6
Jordan <jordan.minecraft.error404@gmail.com>
2026-03-08 22:17:13 +0800

    Add level 25

---
+++ parkour/levels/level25.js
@@ -0,0 +1,30 @@
+window.loadLevelData({
+    playerStart: { x: 50, y: 550 },
+    platforms: [
+        { x: 0, y: 600, width: 200, height: 100 },
+        { x: 230, y: 530, width: 130, height: 20 },
+        { x: 420, y: 460, width: 130, height: 20 },
+        { x: 610, y: 390, width: 130, height: 20 },
+        { x: 800, y: 460, width: 130, height: 20 },
+        { x: 990, y: 530, width: 130, height: 20 },
+        { x: 1150, y: 580, width: 50, height: 20 }
+    ],
+    spikes: [],
+    coins: [
+        { x: 280, y: 480 },
+        { x: 470, y: 410 },
+        { x: 660, y: 340 },
+        { x: 850, y: 410 },
+        { x: 1040, y: 480 }
+    ],
+    goal: { x: 1160, y: 520, width: 40, height: 60 },
+    movingPlatforms: [],
+    enemies: [
+        { x: 240, y: 550, width: 30, height: 30, startX: 240, endX: 350, speed: 3 },
+        { x: 620, y: 420, width: 30, height: 30, startX: 620, endX: 730, speed: 3 },
+        { x: 1000, y: 550, width: 30, height: 30, startX: 1000, endX: 1110, speed: 3 }
+    ],
+    teleporters: [],
+    iceBlocks: [],
+    lava: []
+});
