9b80b3299188405755576cf3ca2247a089b8853d
Jordan <jordan.minecraft.error404@gmail.com>
2026-03-08 22:07:35 +0800

    Add level 30

---
+++ parkour/levels/level30.js
@@ -0,0 +1,25 @@
+window.loadLevelData({
+    playerStart: { x: 50, y: 550 },
+    platforms: [
+        { x: 0, y: 600, width: 200, height: 100 },
+        { x: 280, y: 510, width: 100, height: 20 },
+        { x: 580, y: 480, width: 100, height: 20 },
+        { x: 880, y: 480, width: 100, height: 20 },
+        { x: 1080, y: 550, width: 120, height: 20 }
+    ],
+    spikes: [],
+    coins: [
+        { x: 330, y: 460 },
+        { x: 630, y: 430 },
+        { x: 930, y: 430 },
+        { x: 1130, y: 500 }
+    ],
+    goal: { x: 1130, y: 490, width: 40, height: 60 },
+    movingPlatforms: [],
+    enemies: [],
+    teleporters: [
+        { x: 680, y: 380, width: 40, height: 60, targetX: 780, targetY: 420 }
+    ],
+    iceBlocks: [],
+    lava: []
+});
