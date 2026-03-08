5c284a0bfe50912a64bb56e899d91d53ec1a592a
Jordan <jordan.minecraft.error404@gmail.com>
2026-03-08 22:18:18 +0800

    Add level 38

---
+++ parkour/levels/level38.js
@@ -0,0 +1,26 @@
+window.loadLevelData({
+    playerStart: { x: 50, y: 550 },
+    platforms: [
+        { x: 0, y: 600, width: 200, height: 100 },
+        { x: 300, y: 500, width: 100, height: 20 },
+        { x: 600, y: 460, width: 100, height: 20 },
+        { x: 900, y: 460, width: 100, height: 20 },
+        { x: 1100, y: 550, width: 100, height: 20 }
+    ],
+    spikes: [],
+    coins: [
+        { x: 350, y: 450 },
+        { x: 650, y: 410 },
+        { x: 950, y: 410 },
+        { x: 1140, y: 500 }
+    ],
+    goal: { x: 1140, y: 490, width: 40, height: 60 },
+    movingPlatforms: [],
+    enemies: [],
+    teleporters: [],
+    iceBlocks: [],
+    lava: [
+        { x: 400, y: 540, width: 200, height: 20 },
+        { x: 700, y: 540, width: 200, height: 20 }
+    ]
+});
