8526899112e1f67a8f855d8f57b2d95d346a81de
Jordan <jordan.minecraft.error404@gmail.com>
2026-03-08 22:18:17 +0800

    Add level 37

---
+++ parkour/levels/level37.js
@@ -0,0 +1,26 @@
+window.loadLevelData({
+    playerStart: { x: 50, y: 550 },
+    platforms: [
+        { x: 0, y: 600, width: 200, height: 100 },
+        { x: 280, y: 520, width: 100, height: 20 },
+        { x: 550, y: 480, width: 100, height: 20 },
+        { x: 820, y: 480, width: 100, height: 20 },
+        { x: 1080, y: 560, width: 120, height: 20 }
+    ],
+    spikes: [],
+    coins: [
+        { x: 330, y: 470 },
+        { x: 600, y: 430 },
+        { x: 870, y: 430 },
+        { x: 1130, y: 510 }
+    ],
+    goal: { x: 1130, y: 500, width: 40, height: 60 },
+    movingPlatforms: [],
+    enemies: [],
+    teleporters: [],
+    iceBlocks: [],
+    lava: [
+        { x: 380, y: 560, width: 170, height: 20 },
+        { x: 650, y: 560, width: 170, height: 20 }
+    ]
+});
