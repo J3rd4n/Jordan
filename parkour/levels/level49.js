c5af6d1530f97cad9b47e72cbac5193ec621cde1
Jordan <jordan.minecraft.error404@gmail.com>
2026-03-08 22:19:08 +0800

    Add level 49

---
+++ parkour/levels/level49.js
@@ -0,0 +1,30 @@
+window.loadLevelData({
+    playerStart: { x: 50, y: 550 },
+    platforms: [
+        { x: 0, y: 600, width: 200, height: 100 },
+        { x: 250, y: 530, width: 100, height: 20 },
+        { x: 550, y: 500, width: 100, height: 20 },
+        { x: 850, y: 500, width: 100, height: 20 },
+        { x: 1100, y: 550, width: 100, height: 20 }
+    ],
+    spikes: [
+        { x: 280, y: 500, width: 30, height: 30 },
+        { x: 580, y: 470, width: 30, height: 30 }
+    ],
+    coins: [
+        { x: 300, y: 480 },
+        { x: 600, y: 450 },
+        { x: 900, y: 450 },
+        { x: 1140, y: 500 }
+    ],
+    goal: { x: 1140, y: 490, width: 40, height: 60 },
+    movingPlatforms: [
+        { x: 350, y: 500, width: 100, height: 20, startX: 350, endX: 470, speed: 2 }
+    ],
+    enemies: [],
+    teleporters: [],
+    iceBlocks: [
+        { x: 650, y: 500, width: 200, height: 20 }
+    ],
+    lava: []
+});
