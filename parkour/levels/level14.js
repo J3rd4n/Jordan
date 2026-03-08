41088a3e974204a83b42eb5c8d34c6f8743fcf84
Jordan <jordan.minecraft.error404@gmail.com>
2026-03-08 22:16:22 +0800

    Add level 14

---
+++ parkour/levels/level14.js
@@ -0,0 +1,26 @@
+window.loadLevelData({
+    playerStart: { x: 50, y: 550 },
+    platforms: [
+        { x: 0, y: 600, width: 200, height: 100 },
+        { x: 250, y: 530, width: 100, height: 20 },
+        { x: 550, y: 500, width: 100, height: 20 },
+        { x: 850, y: 500, width: 100, height: 20 },
+        { x: 1100, y: 550, width: 100, height: 20 }
+    ],
+    spikes: [],
+    coins: [
+        { x: 300, y: 480 },
+        { x: 600, y: 450 },
+        { x: 900, y: 450 },
+        { x: 1140, y: 500 }
+    ],
+    goal: { x: 1140, y: 490, width: 40, height: 60 },
+    movingPlatforms: [
+        { x: 350, y: 500, width: 100, height: 20, startX: 350, endX: 470, speed: 3 },
+        { x: 650, y: 470, width: 100, height: 20, startX: 650, endX: 800, speed: 3 }
+    ],
+    enemies: [],
+    teleporters: [],
+    iceBlocks: [],
+    lava: []
+});
