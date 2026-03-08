350a3b7764e133724d2f563fc8b1e111f6efa238
Jordan <jordan.minecraft.error404@gmail.com>
2026-03-08 22:21:31 +0800

    Add level 71

---
+++ parkour/levels/level71.js
@@ -0,0 +1,127 @@
+window.loadLevelData({
+    "playerStart": {
+        "x": 50,
+        "y": 550
+    },
+    "platforms": [
+        {
+            "x": 0,
+            "y": 600,
+            "width": 200,
+            "height": 100
+        },
+        {
+            "x": 394,
+            "y": 551,
+            "width": 100,
+            "height": 20
+        },
+        {
+            "x": 589,
+            "y": 528,
+            "width": 100,
+            "height": 20
+        },
+        {
+            "x": 783,
+            "y": 471,
+            "width": 100,
+            "height": 20
+        },
+        {
+            "x": 978,
+            "y": 448,
+            "width": 100,
+            "height": 20
+        },
+        {
+            "x": 1172,
+            "y": 391,
+            "width": 100,
+            "height": 20
+        },
+        {
+            "x": 1367,
+            "y": 368,
+            "width": 100,
+            "height": 20
+        }
+    ],
+    "spikes": [
+        {
+            "x": 436,
+            "y": 521,
+            "width": 30,
+            "height": 30
+        },
+        {
+            "x": 617,
+            "y": 498,
+            "width": 30,
+            "height": 30
+        },
+        {
+            "x": 807,
+            "y": 441,
+            "width": 30,
+            "height": 30
+        }
+    ],
+    "coins": [
+        {
+            "x": 444,
+            "y": 512
+        },
+        {
+            "x": 639,
+            "y": 486
+        },
+        {
+            "x": 833,
+            "y": 436
+        },
+        {
+            "x": 1028,
+            "y": 410
+        },
+        {
+            "x": 1222,
+            "y": 343
+        },
+        {
+            "x": 1417,
+            "y": 336
+        }
+    ],
+    "goal": {
+        "x": 1397,
+        "y": 358,
+        "width": 40,
+        "height": 60
+    },
+    "movingPlatforms": [
+        {
+            "x": 763,
+            "y": 441,
+            "width": 80,
+            "height": 20,
+            "startX": 746,
+            "endX": 860,
+            "speed": 2.5
+        }
+    ],
+    "enemies": [
+        {
+            "x": 988,
+            "y": 418,
+            "width": 30,
+            "height": 30,
+            "startX": 988,
+            "endX": 1038,
+            "speed": 2.4
+        }
+    ],
+    "teleporters": [],
+    "iceBlocks": [],
+    "lava": []
+});
