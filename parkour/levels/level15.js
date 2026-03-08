96c835abeadd24b94c976761420871e7d3536900
Jordan <jordan.minecraft.error404@gmail.com>
2026-03-08 22:16:23 +0800

    Add level 15

---
+++ parkour/levels/level15.js
@@ -0,0 +1,26 @@
+window.loadLevelData({
+    playerStart: { x: 50, y: 550 },
+    platforms: [
+        { x: 0, y: 600, width: 200, height: 100 },
+        { x: 280, y: 510, width: 100, height: 20 },
+        { x: 580, y: 480, width: 100, height: 20 },
+        { x: 880, y: 480, width: 100, height: 20 },
+        { x: 1080, y: 550, width: 120, height: 20 }
+    ],
+    spikes: [],
+    coins: [
+        { x: 330, y: 460 },
+        { x: 630, y: 430 },
+        { x: 930, y: 430 },
+        { x: 1130, y: 500 }
+    ],
+    goal: { x: 1130, y: 490, width: 40, height: 60 },
+    movingPlatforms: [
+        { x: 380, y: 480, width: 100, height: 20, startX: 380, endX: 500, speed: 3 },
+        { x: 680, y: 450, width: 100, height: 20, startX: 680, endX: 830, speed: 3 }
+    ],
+    enemies: [],
+    teleporters: [],
+    iceBlocks: [],
+    lava: []
+});
