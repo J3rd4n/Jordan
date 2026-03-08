fcd92d92ca35f973453cd123c5d250e7858bfe40
Jordan <jordan.minecraft.error404@gmail.com>
2026-03-08 22:18:42 +0800

    Add level 43

---
+++ parkour/levels/level43.js
@@ -0,0 +1,27 @@
+window.loadLevelData({
+    playerStart: { x: 50, y: 550 },
+    platforms: [
+        { x: 0, y: 600, width: 200, height: 100 },
+        { x: 300, y: 500, width: 100, height: 20 },
+        { x: 600, y: 460, width: 100, height: 20 },
+        { x: 900, y: 460, width: 100, height: 20 },
+        { x: 1100, y: 550, width: 100, height: 20 }
+    ],
+    spikes: [],
+    coins: [
+        { x: 350, y: 450 },
+        { x: 650, y: 410 },
+        { x: 950, y: 410 },
+        { x: 1140, y: 500 }
+    ],
+    goal: { x: 1140, y: 490, width: 40, height: 60 },
+    movingPlatforms: [
+        { x: 400, y: 460, width: 100, height: 20, startX: 400, endX: 520, speed: 2 }
+    ],
+    enemies: [],
+    teleporters: [
+        { x: 700, y: 360, width: 40, height: 60, targetX: 800, targetY: 400 }
+    ],
+    iceBlocks: [],
+    lava: []
+});
