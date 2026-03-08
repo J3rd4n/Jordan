2705b388d30c98d085d400f77c4fab0c3863d82b
Jordan <jordan.minecraft.error404@gmail.com>
2026-03-08 22:02:40 +0800

    Add level 100

---
+++ parkour/levels/level100.js
@@ -0,0 +1,205 @@
+window.loadLevelData({
+    "playerStart": {
+        "x": 50,
+        "y": 550
+    },
+    "platforms": [
+        {
+            "x": 0,
+            "y": 600,
+            "width": 200,
+            "height": 100
+        },
+        {
+            "x": 380,
+            "y": 530,
+            "width": 100,
+            "height": 20
+        },
+        {
+            "x": 560,
+            "y": 550,
+            "width": 100,
+            "height": 20
+        },
+        {
+            "x": 740,
+            "y": 450,
+            "width": 100,
+            "height": 20
+        },
+        {
+            "x": 920,
+            "y": 470,
+            "width": 100,
+            "height": 20
+        },
+        {
+            "x": 1100,
+            "y": 370,
+            "width": 100,
+            "height": 20
+        },
+        {
+            "x": 1280,
+            "y": 390,
+            "width": 100,
+            "height": 20
+        }
+    ],
+    "spikes": [
+        {
+            "x": 432,
+            "y": 500,
+            "width": 30,
+            "height": 30
+        },
+        {
+            "x": 619,
+            "y": 520,
+            "width": 30,
+            "height": 30
+        },
+        {
+            "x": 765,
+            "y": 420,
+            "width": 30,
+            "height": 30
+        },
+        {
+            "x": 940,
+            "y": 440,
+            "width": 30,
+            "height": 30
+        },
+        {
+            "x": 1153,
+            "y": 340,
+            "width": 30,
+            "height": 30
+        },
+        {
+            "x": 425,
+            "y": 500,
+            "width": 30,
+            "height": 30
+        },
+        {
+            "x": 591,
+            "y": 520,
+            "width": 30,
+            "height": 30
+        }
+    ],
+    "coins": [
+        {
+            "x": 430,
+            "y": 482
+        },
+        {
+            "x": 610,
+            "y": 504
+        },
+        {
+            "x": 790,
+            "y": 401
+        },
+        {
+            "x": 970,
+            "y": 439
+        },
+        {
+            "x": 1150,
+            "y": 326
+        },
+        {
+            "x": 1330,
+            "y": 347
+        }
+    ],
+    "goal": {
+        "x": 1310,
+        "y": 380,
+        "width": 40,
+        "height": 60
+    },
+    "movingPlatforms": [
+        {
+            "x": 360,
+            "y": 500,
+            "width": 80,
+            "height": 20,
+            "startX": 339,
+            "endX": 477,
+            "speed": 4
+        },
+        {
+            "x": 540,
+            "y": 520,
+            "width": 80,
+            "height": 20,
+            "startX": 525,
+            "endX": 656,
+            "speed": 4
+        },
+        {
+            "x": 720,
+            "y": 420,
+            "width": 80,
+            "height": 20,
+            "startX": 709,
+            "endX": 847,
+            "speed": 4
+        },
+        {
+            "x": 900,
+            "y": 440,
+            "width": 80,
+            "height": 20,
+            "startX": 878,
+            "endX": 1018,
+            "speed": 4
+        }
+    ],
+    "enemies": [
+        {
+            "x": 570,
+            "y": 520,
+            "width": 30,
+            "height": 30,
+            "startX": 570,
+            "endX": 620,
+            "speed": 3.5
+        },
+        {
+            "x": 750,
+            "y": 420,
+            "width": 30,
+            "height": 30,
+            "startX": 750,
+            "endX": 800,
+            "speed": 3.5
+        },
+        {
+            "x": 930,
+            "y": 440,
+            "width": 30,
+            "height": 30,
+            "startX": 930,
+            "endX": 980,
+            "speed": 3.5
+        },
+        {
+            "x": 1110,
+            "y": 340,
+            "width": 30,
+            "height": 30,
+            "startX": 1110,
+            "endX": 1160,
+            "speed": 3.5
+        }
+    ],
+    "teleporters": [],
+    "iceBlocks": [],
+    "lava": []
+});
