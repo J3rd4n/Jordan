c152f392d67e2e113c9c11078858cc91e498bf87
Jordan <jordan.minecraft.error404@gmail.com>
2026-03-08 22:09:46 +0800

    Add level 6

---
+++ parkour/levels/level6.js
@@ -0,0 +1,29 @@
+window.loadLevelData({
+    playerStart: { x: 50, y: 550 },
+    platforms: [
+        { x: 0, y: 600, width: 200, height: 100 },
+        { x: 250, y: 550, width: 150, height: 20 },
+        { x: 450, y: 500, width: 150, height: 20 },
+        { x: 650, y: 450, width: 150, height: 20 },
+        { x: 850, y: 500, width: 150, height: 20 },
+        { x: 1050, y: 550, width: 150, height: 20 }
+    ],
+    spikes: [
+        { x: 280, y: 520, width: 30, height: 30 },
+        { x: 480, y: 470, width: 30, height: 30 },
+        { x: 680, y: 420, width: 30, height: 30 },
+        { x: 880, y: 470, width: 30, height: 30 }
+    ],
+    coins: [
+        { x: 300, y: 500 },
+        { x: 500, y: 450 },
+        { x: 700, y: 400 },
+        { x: 900, y: 450 }
+    ],
+    goal: { x: 1150, y: 490, width: 40, height: 60 },
+    movingPlatforms: [],
+    enemies: [],
+    teleporters: [],
+    iceBlocks: [],
+    lava: []
+});
