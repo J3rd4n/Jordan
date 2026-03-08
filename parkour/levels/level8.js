ef19aba3d9adb86ef8e662af0106e82fed416795
Jordan <jordan.minecraft.error404@gmail.com>
2026-03-08 22:12:14 +0800

    Add level 8

---
+++ parkour/levels/level8.js
@@ -0,0 +1,35 @@
+window.loadLevelData({
+    playerStart: { x: 50, y: 550 },
+    platforms: [
+        { x: 0, y: 600, width: 200, height: 100 },
+        { x: 220, y: 500, width: 100, height: 20 },
+        { x: 370, y: 450, width: 100, height: 20 },
+        { x: 520, y: 400, width: 100, height: 20 },
+        { x: 670, y: 350, width: 100, height: 20 },
+        { x: 820, y: 400, width: 100, height: 20 },
+        { x: 970, y: 450, width: 100, height: 20 },
+        { x: 1120, y: 500, width: 80, height: 20 }
+    ],
+    spikes: [
+        { x: 250, y: 470, width: 30, height: 30 },
+        { x: 400, y: 420, width: 30, height: 30 },
+        { x: 550, y: 370, width: 30, height: 30 },
+        { x: 700, y: 320, width: 30, height: 30 },
+        { x: 850, y: 370, width: 30, height: 30 },
+        { x: 1000, y: 420, width: 30, height: 30 }
+    ],
+    coins: [
+        { x: 250, y: 450 },
+        { x: 400, y: 400 },
+        { x: 550, y: 350 },
+        { x: 700, y: 300 },
+        { x: 850, y: 350 },
+        { x: 1000, y: 400 }
+    ],
+    goal: { x: 1140, y: 440, width: 40, height: 60 },
+    movingPlatforms: [],
+    enemies: [],
+    teleporters: [],
+    iceBlocks: [],
+    lava: []
+});
