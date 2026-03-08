e85a029671aad3e86793b1a1cd59fd923c14f72f
Jordan <jordan.minecraft.error404@gmail.com>
2026-03-08 22:11:23 +0800

    Add level 7

---
+++ parkour/levels/level7.js
@@ -0,0 +1,32 @@
+window.loadLevelData({
+    playerStart: { x: 50, y: 550 },
+    platforms: [
+        { x: 0, y: 600, width: 200, height: 100 },
+        { x: 230, y: 520, width: 120, height: 20 },
+        { x: 400, y: 480, width: 120, height: 20 },
+        { x: 570, y: 440, width: 120, height: 20 },
+        { x: 740, y: 480, width: 120, height: 20 },
+        { x: 910, y: 520, width: 120, height: 20 },
+        { x: 1080, y: 560, width: 120, height: 20 }
+    ],
+    spikes: [
+        { x: 260, y: 490, width: 30, height: 30 },
+        { x: 430, y: 450, width: 30, height: 30 },
+        { x: 600, y: 410, width: 30, height: 30 },
+        { x: 770, y: 450, width: 30, height: 30 },
+        { x: 940, y: 490, width: 30, height: 30 }
+    ],
+    coins: [
+        { x: 270, y: 470 },
+        { x: 440, y: 430 },
+        { x: 610, y: 390 },
+        { x: 780, y: 430 },
+        { x: 950, y: 470 }
+    ],
+    goal: { x: 1120, y: 500, width: 40, height: 60 },
+    movingPlatforms: [],
+    enemies: [],
+    teleporters: [],
+    iceBlocks: [],
+    lava: []
+});
