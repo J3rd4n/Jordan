0876f8559930cf9aa3478be52d099a7d2c803b45
Jordan <jordan.minecraft.error404@gmail.com>
2026-03-08 22:05:58 +0800

    Add level 2

---
+++ parkour/levels/level2.js
@@ -0,0 +1,26 @@
+window.loadLevelData({
+    playerStart: { x: 50, y: 550 },
+    platforms: [
+        { x: 0, y: 600, width: 200, height: 100 },
+        { x: 230, y: 520, width: 120, height: 20 },
+        { x: 400, y: 480, width: 120, height: 20 },
+        { x: 570, y: 440, width: 120, height: 20 },
+        { x: 740, y: 480, width: 120, height: 20 },
+        { x: 910, y: 520, width: 120, height: 20 },
+        { x: 1080, y: 560, width: 120, height: 20 }
+    ],
+    spikes: [],
+    coins: [
+        { x: 270, y: 470 },
+        { x: 440, y: 430 },
+        { x: 610, y: 390 },
+        { x: 780, y: 430 },
+        { x: 950, y: 470 }
+    ],
+    goal: { x: 1120, y: 500, width: 40, height: 60 },
+    movingPlatforms: [],
+    enemies: [],
+    teleporters: [],
+    iceBlocks: [],
+    lava: []
+});
