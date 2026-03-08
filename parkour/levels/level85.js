f8acba634105467ac1fed0102e0e547ed601b9a7
Jordan <jordan.minecraft.error404@gmail.com>
2026-03-08 22:22:28 +0800

    Add level 85

---
+++ parkour/levels/level85.js
@@ -0,0 +1,157 @@
+window.loadLevelData({
+    "playerStart": {
+        "x": 50,
+        "y": 550
+    },
+    "platforms": [
+        {
+            "x": 0,
+            "y": 600,
+            "width": 200,
+            "height": 100
+        },
+        {
+            "x": 387,
+            "y": 541,
+            "width": 100,
+            "height": 20
+        },
+        {
+            "x": 575,
+            "y": 538,
+            "width": 100,
+            "height": 20
+        },
+        {
+            "x": 762,
+            "y": 461,
+            "width": 100,
+            "height": 20
+        },
+        {
+            "x": 950,
+            "y": 458,
+            "width": 100,
+            "height": 20
+        },
+        {
+            "x": 1137,
+            "y": 381,
+            "width": 100,
+            "height": 20
+        },
+        {
+            "x": 1325,
+            "y": 378,
+            "width": 100,
+            "height": 20
+        }
+    ],
+    "spikes": [
+        {
+            "x": 444,
+            "y": 511,
+            "width": 30,
+            "height": 30
+        },
+        {
+            "x": 613,
+            "y": 508,
+            "width": 30,
+            "height": 30
+        },
+        {
+            "x": 816,
+            "y": 431,
+            "width": 30,
+            "height": 30
+        },
+        {
+            "x": 987,
+            "y": 428,
+            "width": 30,
+            "height": 30
+        },
+        {
+            "x": 1184,
+            "y": 351,
+            "width": 30,
+            "height": 30
+        }
+    ],
+    "coins": [
+        {
+            "x": 437,
+            "y": 508
+        },
+        {
+            "x": 625,
+            "y": 502
+        },
+        {
+            "x": 812,
+            "y": 422
+        },
+        {
+            "x": 1000,
+            "y": 418
+        },
+        {
+            "x": 1187,
+            "y": 333
+        },
+        {
+            "x": 1375,
+            "y": 343
+        }
+    ],
+    "goal": {
+        "x": 1355,
+        "y": 368,
+        "width": 40,
+        "height": 60
+    },
+    "movingPlatforms": [
+        {
+            "x": 555,
+            "y": 508,
+            "width": 80,
+            "height": 20,
+            "startX": 541,
+            "endX": 673,
+            "speed": 3.2
+        },
+        {
+            "x": 930,
+            "y": 428,
+            "width": 80,
+            "height": 20,
+            "startX": 908,
+            "endX": 1038,
+            "speed": 3.2
+        }
+    ],
+    "enemies": [
+        {
+            "x": 772,
+            "y": 431,
+            "width": 30,
+            "height": 30,
+            "startX": 772,
+            "endX": 822,
+            "speed": 2.9
+        },
+        {
+            "x": 960,
+            "y": 428,
+            "width": 30,
+            "height": 30,
+            "startX": 960,
+            "endX": 1010,
+            "speed": 2.9
+        }
+    ],
+    "teleporters": [],
+    "iceBlocks": [],
+    "lava": []
+});
